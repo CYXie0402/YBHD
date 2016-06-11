a6296efdac28c96c81646866a8bfd1ec38c7d0f3
CYXie0402 <349372895@qq.com>
2016-06-11 15:55:31 +0800

    xz
    xz

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  约吧互动
+//
+//  Created by iMac on 16/6/1.
+//  Copyright © 2016年 xingzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
